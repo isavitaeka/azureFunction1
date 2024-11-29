53e1bfc2c8f42fced41406ac2de96141be66979e
i.savita.eka <i.savita.eka@gmail.com>
2024-11-29 15:54:25 +0000

    Updated function_app.py

--- function_app.py
+++ function_app.py
@@ -9,6 +9,8 @@ def GetDetails(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    job = req.params.get('job')
+
     if not name:
         try:
             req_body = req.get_json()
@@ -17,10 +19,19 @@ def GetDetails(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+    if not job:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+    if not job:
+            name = req_body.get('name')
+
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name} with job as {job}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name and job in the query string or in the request body for a personalized response.",
              status_code=200
         )
