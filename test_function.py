a11080cfbfdd50ca6a64013ec7c3f3eeb17ebf20
i.savita.eka <i.savita.eka@gmail.com>
2024-12-03 09:19:22 +0000

    Updated test_function.py

--- test_function.py
+++ test_function.py
@@ -4,12 +4,12 @@ import requests
 class TestAzureFunction(unittest.TestCase):
 
     def test_get_details_with_query_params(self):
-        response = requests.get('http://localhost:7071/api/GetDetails', params={'name': 'John', 'job': 'Developer'})
+        response = requests.get('https://shop-function.azurewebsites.net/api/GetDetails?code=905FC_Pw3vqF1NFNiJItHBLBu57AU2hfa8j1EB66K-sCAzFuAYJshw%3D%3D', params={'name': 'John', 'job': 'Developer'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "Hello, John with job as Developer. This HTTP triggered function executed successfully.")
 
     def test_get_details_without_query_params(self):
-        response = requests.get('http://localhost:7071/api/GetDetails')
+        response = requests.get('https://shop-function.azurewebsites.net/api/GetDetails?code=905FC_Pw3vqF1NFNiJItHBLBu57AU2hfa8j1EB66K-sCAzFuAYJshw%3D%3D')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "This HTTP triggered function executed successfully. Pass a name and job in the query string or in the request body for a personalized response.")
 
